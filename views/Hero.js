0b115d1e0d30b7c2e4d3cf8b5fba3e465706981d
NiceTwice <onlynicetwice@gmail.com>
2023-03-08 16:45:29 -0300

    new cv link

--- views/Hero.js
+++ views/Hero.js
@@ -23,7 +23,7 @@ const Hero = () => {
                                 <BsGithub className="hover:scale-105 cursor-pointer" size={30}/>
                             </Tooltip>
                         </a>
-                        <a href="https://drive.google.com/file/d/1raa4tqOCRZ5CbzXUQLBkHO6JNpKjyLmI/view?usp=share_link" target="_blank" rel="noreferrer" className="text-base text-white font-sans flex justify-center items-center p-3 bg-rose-800 hover:bg-rose-800/90 transition-colors">
+                        <a href="https://drive.google.com/file/d/1uW9cHgDrMvBN_--3CRHta-7dL4KVljg4/view?usp=share_link" target="_blank" rel="noreferrer" className="text-base text-white font-sans flex justify-center items-center p-3 bg-rose-800 hover:bg-rose-800/90 transition-colors">
                             My resume
                             <HiDownload className="ml-2 cursor-pointer text-white" size={20}/>
                         </a>
